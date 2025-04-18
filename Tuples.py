a8cb5f505e19db90d6029f29e9fa0c6e456f3c15
Battfly-q <guqi187@gmail.com>
2025-04-19 00:22:59 +0800

    4.19

---
+++ Tuples.py
@@ -0,0 +1,9 @@
+# 有序的、不可更改的，并且允许重复的值
+
+thistuple = ("apple", "banana", "cherry")
+thistuple = ["apple", "banana", "cherry"]
+print(thistuple[0])
+print()
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
