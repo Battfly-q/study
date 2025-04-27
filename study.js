ccef6d8231926a19b61be9bcd9631629f6381eb0
Battfly-q <guqi187@gmail.com>
2025-04-27 16:28:09 +0800

    1

---
+++ study.js
@@ -0,0 +1,21 @@
+console.log(typeof 4.5)
+console.log(typeof "x")
+console.log(-(10 - 2))
+console.log(3>2)
+console.log(3<2)
+console.log("">"")
+//不等于 ！=   不等于他自己
+console.log(NaN == NaN)
+console.log(true&&false)
+console.log(true&&true)
+console.log(true||true)
+//条件运算符
+console.log(true?1:2)
+//自动类型转换
+console.log(8*null)
+//绑定
+let ten = 10
+console.log(ten * ten)
+
+let one = 1,two = 2
+console.log(one+two)
